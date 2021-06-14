9b5f42d768e787b6aba013b80070c65e1e0bf1d8
Alejandro Reyes O <Alejandro.reyes.o@outlook.com>
2021-06-14 18:20:54 -0400

    Generar interfaz de ver consolas y agregar consolas

--- web/zgames/resources/views/layouts/master.blade.php
+++ web/zgames/resources/views/layouts/master.blade.php
@@ -6,17 +6,19 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token" content="{{csrf_token()}}"> <!--Esto es para que se puedan hacer peticiones desde 
+    la parte web hacia el servidor-->
+    
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <title>Zgames</title>
   </head>
   <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-warning">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#"><img src="{{asset('img/logo.png')}}" class="img-fluid logo" class="logo"></a>
+              <a class="navbar-brand" href="{{route('home')}}"><img src="{{asset('img/logo.png')}}" class="img-fluid logo" class="logo"></a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -38,7 +40,8 @@
     
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="{{asset('js/axios_config.js')}}"></script>
 
   </body>
 </html>
